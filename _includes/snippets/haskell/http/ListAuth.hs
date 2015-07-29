32e77002a3357eb784b0e435e941585a9796e000
justusadam <dev@justus.science>
2015-07-30 01:54:54 +0200

    Slight code layout change.

--- _includes/snippets/haskell/http/ListAuth.hs
+++ _includes/snippets/haskell/http/ListAuth.hs
@@ -1,14 +1,15 @@
 import HTTP.Browser
+import Control.Arrow (second)
 
 
-authList =
-  [ (fromJust $ parseURI "http://google.com", ("walter", "12.24.1975"))
-  , (fromJust $ parseURI "http://facebook.com", ("MarkZuckerberg", "IamTheFounder"))
-  , (fromJust $ parseURI "https://reddit.com", ("StephenHawking", "BlackHole"))
+authList = map (second (fromJust . parseURI))  -- makes the strings into URI's
+  [ ("http://google.com"  , ("walter", "12.24.1975"))
+  , ("http://facebook.com", ("MarkZuckerberg", "IamTheFounder"))
+  , ("http://reddit.com"  , ("StephenHawking", "BlackHole"))
   ]
 
 
 main =
   browse $ do
-    setAuthorityGen (\uri _ -> return $ lookup uri authList)
+    setAuthorityGen (const . return . flip lookup authList)
     request $ getRequest "http://github.com"
