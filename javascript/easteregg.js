139bd23a2ec144ff01a4d0850b8c817628e7fb58
Christoph Kepler <development@kepler.international>
2016-10-11 11:28:33 +0200

    Point finders of the easteregg to source code repo

--- javascript/easteregg.js
+++ javascript/easteregg.js
@@ -9,6 +9,7 @@ if(window.console || "console" in window) {
                     \\/                  \\/  \\/         \\/     \\/        \\/     \\/     \\/    \\/ `, "color:#3A8DE5; font-family:monospace");
 	console.log(`%c Welcome to my site`, "color:#FFA13C; font-size:16px; font-family:monospace; font-weight: bold");
 	console.log(`%c I like your enthusiasm but here you won't find any content here`, "color:#194D90; font-size:12px; font-family:monospace");
+	console.log(`%c If you are interested in the source-code go to github.com/JustusAdam/justusadam.github.io`, "color:#194D90; font-size:12px; font-family:monospace");
 }
 }
 window.onload = easteregg;
